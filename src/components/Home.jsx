3ba3266bb326edbd2bda5c5d917b3f2d5da0dfac
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-03-28 10:13:16 +0100

    add hero section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,16 @@
-import Header from "./Header";
-
 const HomePage = () => {
-  return <Header />;
+  return (
+    <>
+      <main>
+        <div className="hero-container">
+          <picture>
+            <img src="./assets/img/hero-image.webp" alt="" />
+          </picture>
+          <h2>Come as costumer stay as family!</h2>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default HomePage;
