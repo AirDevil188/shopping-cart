39304da53d0e1df19257ab1e5e174012cb54a709
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-03-28 10:29:24 +0100

    cart template

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,3 +1,16 @@
-const Cart = () => {};
+const Cart = () => {
+  return (
+    <section id="shopping-cart-section">
+      <div>
+        <h2>Your Cart (0 items)</h2>
+        <div className="shopping-cart-items"></div>
+        <div className="subtotal-container">
+          <h3>Subtotal: </h3>
+          <button className="checkout-button">Checkout</button>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Cart;
