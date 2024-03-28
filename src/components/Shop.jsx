047a2e01ee57d1dcf2b4358dbf741d8f95ce8250
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-03-28 11:06:30 +0100

    shop template

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,9 +1,23 @@
 const Shop = () => {
   return (
-    <div>
-      <h1>Shop Page!</h1>
-      <p>So, how are you?</p>
-    </div>
+    <section id="shop-section">
+      <div className="hero-text-container">
+        <h2>Check out our awesome new items!</h2>
+        <div className="items-container">
+          <figure id="item-1">
+            <picture>
+              <img src="" alt="" />
+            </picture>
+            <h4 className="item-name"></h4>
+            <figcaption className="item-caption"></figcaption>
+            <div className="button-container">
+              <input type="number" />
+              <button className="btn-add-to-cart">Add To Cart</button>
+            </div>
+          </figure>
+        </div>
+      </div>
+    </section>
   );
 };
 
