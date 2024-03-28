8bb66718f3b6063a9998bf5e2afd026aea9e4339
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-03-28 09:48:10 +0100

    fixed routher pathing for root /

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,14 +2,18 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
 import Shop from "../components/Shop";
 import ErrorPage from "./ErrorPage";
+import HomePage from "../components/Home";
 
 const Router = () => {
   const router = createBrowserRouter([
     {
-      path: "/",
       element: <App />,
       errorElement: <ErrorPage />,
       children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
         {
           path: "/shop",
           element: <Shop />,
