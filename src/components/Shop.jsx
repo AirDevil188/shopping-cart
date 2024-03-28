1359fa8b4e2cdf5d176b14c163932c0d1cd6994d
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-03-28 22:57:05 +0100

    product section add

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,23 +1,48 @@
+import { useEffect, useState } from "react";
+import Products from "./Products";
+import { getRequestFetch } from "../helper/fetchData";
+
 const Shop = () => {
   return (
     <section id="shop-section">
       <div className="hero-text-container">
         <h2>Check out our awesome new items!</h2>
         <div className="items-container">
-          <figure id="item-1">
-            <picture>
-              <img src="" alt="" />
-            </picture>
-            <h4 className="item-name"></h4>
-            <figcaption className="item-caption"></figcaption>
-            <div className="button-container">
-              <input type="number" />
-              <button className="btn-add-to-cart">Add To Cart</button>
-            </div>
-          </figure>
+          <ProductSection />
         </div>
       </div>
     </section>
+  );
+};
+
+const ProductSection = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const data = await getRequestFetch(
+          "https://fakestoreapi.com/products?limit=5"
+        );
+        setData(data);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <>
+      {loading && <p>Loading posts...</p>}
+      {error && <p>{error}</p>}
+      {data && <Products data={data} />}
+    </>
   );
 };
 
