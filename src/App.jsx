27166f848c39c5e1ca2176bffd7d6431949cdc2d
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-03-31 12:08:09 +0200

    moved cart state to my App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { useState } from "react";
 
 const App = () => {
+  const [cart, setCart] = useState([]);
   return (
     <>
       <Header />
       <Footer />
-      <Outlet />
+      <Outlet context={[cart, setCart]} />
     </>
   );
 };
