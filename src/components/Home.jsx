d42b21a7590eef511f2e07fa642ce44a121e4f13
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-04-11 22:47:38 +0200

    fix image to fit 100% of content page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,33 @@
+import styled from "styled-components";
+
 const HomePage = () => {
   return (
     <>
       <main>
-        <div className="hero-container">
+        <StyledHeroContainer>
           <picture>
-            <img src="./assets/img/hero-image.webp" alt="" />
+            <StyledHeroPicture
+              src="../public/assets/img/hero-image.webp"
+              alt="Shop open sign"
+            ></StyledHeroPicture>
           </picture>
           <h2>Come as costumer stay as family!</h2>
-        </div>
+        </StyledHeroContainer>
       </main>
     </>
   );
 };
 
 export default HomePage;
+
+//styled-components
+
+const StyledHeroContainer = styled.div`
+  display: flex;
+  flex-flow: column wrap;
+  align-items: center;
+`;
+
+const StyledHeroPicture = styled.img`
+  width: 100%;
+`;
