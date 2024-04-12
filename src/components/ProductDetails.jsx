7cc86e9459d74150b0111844a8827981f864feaf
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-04-12 17:23:29 +0200

    added styling to single product page

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useOutletContext, useParams } from "react-router-dom";
 import getData from "../helper/getData";
 import ErrorPage from "../routes/ErrorPage";
+import styled from "styled-components";
 
 const ProductDetails = () => {
   const { productID } = useParams();
@@ -36,35 +37,36 @@ const ProductDetails = () => {
       {data &&
         data.map((data) => {
           return (
-            <section className="product-section" key={data.id}>
+            <StyledProductSection key={data.id}>
               <figure key={data.id}>
-                <div className="product-title">
-                  <h3>{data.title}</h3>
-                </div>
                 <div className="product-image">
                   <picture>
                     <img src={data.image} alt="" />
                   </picture>
                 </div>
+                <div className="product-title">
+                  <h3>{data.title}</h3>
+                </div>
                 <div className="product-description">
                   <p title="prod-desc">{data.description}</p>
                 </div>
                 <div className="price-container">
-                  <output>{data.price}</output>
+                  <output>${data.price}</output>
                 </div>
+                <input
+                  type="number"
+                  id={data.id}
+                  onChange={(event) => setValue(Number(event.target.value))}
+                  min={1}
+                  value={1}
+                />
                 <div className="buttons-container">
-                  <input
-                    type="number"
-                    id={data.id}
-                    onChange={(event) => setValue(Number(event.target.value))}
-                    min={1}
-                  />
                   <button onClick={handleAddToCart} type="submit" id={data.id}>
                     ADD TO CART
                   </button>
                 </div>
               </figure>
-            </section>
+            </StyledProductSection>
           );
         })}
     </>
@@ -72,3 +74,54 @@ const ProductDetails = () => {
 };
 
 export default ProductDetails;
+
+const StyledProductSection = styled.section`
+  section {
+    display: flex;
+    flex-flow: column wrap;
+    justify-content: center;
+    align-items: center;
+  }
+
+  figure {
+    display: flex;
+    flex-flow: column wrap;
+    gap: 20px;
+    align-items: center;
+    padding: 1rem;
+    text-align: center;
+  }
+
+  img {
+    max-width: 450px;
+  }
+
+  output {
+    font-family:
+      system-ui,
+      -apple-system,
+      BlinkMacSystemFont,
+      "Segoe UI",
+      Roboto,
+      Oxygen,
+      Ubuntu,
+      Cantarell,
+      "Open Sans",
+      "Helvetica Neue",
+      sans-serif;
+    font-size: 1.1rem;
+    font-weight: 900;
+  }
+
+  button {
+    background-color: #000;
+    padding: 1rem;
+    text-transform: uppercase;
+    color: white;
+    border: 1px solid white;
+    cursor: pointer;
+  }
+  input {
+    max-width: 50px;
+  }
+`;
